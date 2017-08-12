91d9756225e3de2557a90b3b6154757f162747e6
codeinaire <johncodeinaire@gmail.com>
2017-08-12 15:39:29 +1000

    Delete search folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './presentational/navbar/navbar'
 import style from './App.css'
-import Search from './presentational/search/Search'
+import Search from './presentational/search/SearchBox'
 
 class App extends Component {
   render() {
     return (
       <div className="background mobile-only">
         <Navbar />
+        <Search />
       </div>
     );
   }
