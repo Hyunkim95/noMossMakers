5ab4c53278fa26a644125c16b2cb8aa0da95a023
Hyun Kim <hkim@live.com.au>
2017-08-12 14:14:33 +1000

    added background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import style from './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="background">
-        <h2>HELLO</h2>
       </div>
     );
   }
