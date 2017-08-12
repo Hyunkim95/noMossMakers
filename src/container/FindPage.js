f94d823085ea6e9db038001040a7993a9a990219
Hyun Kim <hkim@live.com.au>
2017-08-12 21:51:24 +1000

    added some styling to find page:

--- src/container/FindPage.js
+++ src/container/FindPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import style from './FindPage.css'
+import NavBar from '../presentational/navbar/navbar.js';
 
 class FindPage extends Component {
   render() {
     return(
-      <div>
-        <h1>FIND </h1>
+      <div className="background">
+        <NavBar />
+        <div className="input-form find-text">
+          <h2>Hi there,</h2>
+          <h3 className="signup-text">Let's find you a doctor</h3>
+        </div>
       </div>
     )
   }
