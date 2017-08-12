fd0a8ac699857856af4940fbf9f876a9d1dccd81
Hyun Kim <hkim@live.com.au>
2017-08-12 23:17:39 +1000

    merrged

--- src/container/AppointmentPage.js
+++ src/container/AppointmentPage.js
@@ -23,19 +23,3 @@ class AppointmentPage extends Component {
 }
 
 export default AppointmentPage;
-
-import React, { Component } from 'react';
-
-
-class SelectPage extends Component {
-  render() {
-    return(
-      <div>
-        <h1>Select</h1>
-
-      </div>
-    )
-  }
-}
-
-export default SelectPage;
--- src/container/FindPage.js
+++ src/container/FindPage.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import style from './FindPage.css'
+import style from './FindPage.css';
+import { Link } from 'react-router-dom'
 import NavBar from '../presentational/navbar/navbar.js';
 
 class FindPage extends Component {
   render() {
     return(
-<<<<<<< HEAD
-      <div>
-        <h1>FIND </h1>
-        <Link to="/select">
-          <SelectButton basic size='large'>
-            <h4>SELECT CLINIC</h4>
-          </SelectButton>
-        </Link>
-=======
       <div className="background">
         <NavBar />
         <div className="input-form find-text">
           <h2>Hi there,</h2>
           <h3 className="signup-text">Let's find you a doctor</h3>
         </div>
->>>>>>> f94d823085ea6e9db038001040a7993a9a990219
+        <Link to="/select">
+          <SelectButton basic size='large'>
+            <h4>SELECT CLINIC</h4>
+          </SelectButton>
+        </Link>
       </div>
     )
   }
