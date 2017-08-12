df4b297a9123daa7bf86d9ef7a387b68fca6aa94
Hyun Kim <hkim@live.com.au>
2017-08-12 14:27:32 +1000

    added basic layout for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './presentational/navbar/navbar'
 import style from './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="background">
+      <div className="background mobile-only">
+        <Navbar />
       </div>
     );
   }
